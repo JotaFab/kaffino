3376598161e873bb9c6f146734a6612617017642
JotaFab <fabrizioore512@gmail.com>
2025-03-04 03:37:15 -0500

    Refactor database initialization to improve table definitions and constraints

--- internal/database/init.go
+++ internal/database/init.go
@@ -40,7 +40,7 @@ func (s *service) createKaffinoTables() error {
 				subscriber BOOLEAN DEFAULT FALSE, -- Indicates if the user is a subscriber
 				username VARCHAR(255),       -- User's username
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 			);
 						
 			CREATE TABLE IF NOT EXISTS products (
@@ -49,19 +49,20 @@ func (s *service) createKaffinoTables() error {
 				images TEXT,             -- Comma-separated list of image URLs
 				title VARCHAR(255) NOT NULL,
 				description TEXT,
+				Tags Text,
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 			);
 				
 			CREATE TABLE IF NOT EXISTS inventory (
 				id VARCHAR(36) PRIMARY KEY,
 				product_id VARCHAR(36) NOT NULL,
 				stock INTEGER NOT NULL DEFAULT 0,
-				sizes TEXT,               -- Comma-separated list of available sizes
+				size TEXT,               -- Comma-separated list of available sizes
 				price DECIMAL(10,2) NOT NULL DEFAULT 0.0,
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-				FOREIGN KEY (product_id) REFERENCES products(id),
+				FOREIGN KEY (product_id) REFERENCES products(id)
 			);
 
 			CREATE TABLE IF NOT EXISTS orders (
@@ -75,7 +76,7 @@ func (s *service) createKaffinoTables() error {
 				order_status VARCHAR(255) DEFAULT 'Pending', -- Order status (e.g., "Pending", "Processing", "Shipped", "Delivered", "Cancelled")
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-				FOREIGN KEY (user_id) REFERENCES users(id), -- Foreign key to the users table
+				FOREIGN KEY (user_id) REFERENCES users(id) -- Foreign key to the users table
 			);
 
 			CREATE TABLE IF NOT EXISTS order_items (
@@ -87,7 +88,7 @@ func (s *service) createKaffinoTables() error {
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				FOREIGN KEY (order_id) REFERENCES orders(id), -- Foreign key to the orders table
-				FOREIGN KEY (product_id) REFERENCES products(id), -- Foreign key to the products table
+				FOREIGN KEY (product_id) REFERENCES products(id) -- Foreign key to the products table
 			);
 
 			CREATE TABLE IF NOT EXISTS reviews (
@@ -99,7 +100,7 @@ func (s *service) createKaffinoTables() error {
 				created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 				FOREIGN KEY (product_id) REFERENCES products(id),
-				FOREIGN KEY (user_id) REFERENCES users(id),
+				FOREIGN KEY (user_id) REFERENCES users(id)
 			);
 		`
 
